aec01317ca1a95668679b961915959b9f4ab05ae
최찬혁 <cksgurwkd12@naver.com>
2023-03-07 01:05:23 +0000

    feat: functianl interface and lambda

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,6 +2,8 @@ package project;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("TEST");
+        FunctionalInterfaceAdmin.run();
+        FunctionalInterfaceJava.run();
+
     }
 }
